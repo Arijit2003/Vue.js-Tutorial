5cb710bf1cebed5cb0012adbad9a5a2b4d3a3068
Arijit Modak <arijitmo+Zeta@zeta.tech>
2025-08-04 17:26:08 +0530

    2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <p v-bind:class="[firstStyle,mySecondStyle]">
+    <p v-bind:style="myStyles">
       {{"Hello Mr." + myName }}
     </p>
 </template>
@@ -8,21 +8,17 @@
 
 <script setup>
   import { ref } from 'vue';
-  const firstStyle = ref('firstStyle'); 
-  const mySecondStyle = ref('secondStyle')
+  const myStyles = ref({
+    backgroundColor: 'blue',
+    color: 'red',
+    fontSize: '42px'
+  }); 
   const myName = ref('Arijit M.');
 
 </script>
 
 
 <style scoped>
-  .firstStyle {
-    color: red;
-    text-decoration: underline;
-    background-color: blue;
-  }
-  .secondStyle {
-    background-color: blue;
-  }
+
 
 </style>
