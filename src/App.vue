79fe9ec48ef983dc71eac0afce9c7b309c47f5bb
Arijit Modak <arijitmo+Zeta@zeta.tech>
2025-08-04 17:36:56 +0530

    3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 
 <template>
-    <p v-bind:style="myStyles">
-      {{"Hello Mr." + myName }}
-    </p>
+    <input type="text" ref = "txt1" /> </br>
+    <button type = "button" @click = "show">Greet</button>
 </template>
 
 
 <script setup>
+// local variable and template ref variable name should be same
   import { ref } from 'vue';
-  const myStyles = ref({
-    backgroundColor: 'blue',
-    color: 'red',
-    fontSize: '42px'
-  }); 
-  const myName = ref('Arijit M.');
+  const txt1 = ref('txt1');
+  const show = () =>{
+    // txt1  is a ref object, txt1.value refers to the input element
+    alert(txt1.value.value);
+  }
 
 </script>
 
