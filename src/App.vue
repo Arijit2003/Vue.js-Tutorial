26926eb37b522dad11876c8be84c63f765cb284a
Arijit Modak <arijitmo+Zeta@zeta.tech>
2025-08-04 17:43:16 +0530

    4, 2 way binding

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 
 <template>
-    <input type="text" ref = "txt1" /> </br>
+    <input type="text" v-model = "myName" /> </br>
     <button type = "button" @click = "show">Greet</button>
+    {{ myName }}
 </template>
 
 
 <script setup>
-// local variable and template ref variable name should be same
+
   import { ref } from 'vue';
-  const txt1 = ref('txt1');
+  const myName = ref('Arijit Modak');
   const show = () =>{
-    // txt1  is a ref object, txt1.value refers to the input element
     alert(txt1.value.value);
   }
 
