fbbe90945be905422cee947fc626e31a83c79c56
Arijit Modak <arijitmo+Zeta@zeta.tech>
2025-08-04 17:59:34 +0530

    Calculator

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,38 @@
 
 <template>
-    <input type="text" :value = "count" /> </br>
-    <button type = "button" @click = "increment">+</button>
-    <button type = "button" @click = "decrement">-</button>
+
+  <table border="1">
+    <tbody>
+
+    <tr>
+      <th>First Number</th>
+      <td> <input type="text" ref="fnum"/></td>
+    </tr>
+
+    <tr>
+      <th>Second Number</th>
+      <td> <input type="text" ref="snum"/></td>
+    </tr>
+
+
+    <tr>
+      <th>Results</th>
+      <td> <input type="text" ref="res"/></td>
+    </tr>
+
+    <tr>
+      <td><button type="button" @click="sum">Add</button></td>
+      <td><button type="button" @click="sub">Sub</button></td>
+    </tr>
+
+    <tr>
+      <td><button type="button" @click="mul">Mul</button></td>
+      <td><button type="button" @click="div">Div</button></td>
+    </tr>
+
+  </tbody>
+
+  </table>
 
 </template>
 
@@ -11,19 +41,36 @@
 
   import { ref } from 'vue';
   
-  const count = ref(0);
+
+  const fnum = ref('fnum');
+  const snum = ref('snum');
+  const res = ref('res');
+
+  const  sum = () => {
+    res.value.value = parseFloat(fnum.value.value) + parseFloat(snum.value.value);
+  }
+
+  const  sub = () => {
+    res.value.value = parseFloat(fnum.value.value) - parseFloat(snum.value.value);
+  }
+
+  const  mul = () => {
+    res.value.value = parseFloat(fnum.value.value) * parseFloat(snum.value.value);
+  }
+
+  const  div = () => {
+    res.value.value = parseFloat(fnum.value.value) / parseFloat(snum.value.value);
+  }
   
-  const increment = () => {
-    count.value ++;
-  }
-  const decrement = () => {
-    count.value --;
-  }
 
 </script>
 
 
 <style scoped>
 
+  table, th, td {
+    border-collapse: collapse;
+    border: 1px solid black;
+  }
 
 </style>
