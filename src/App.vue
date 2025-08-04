db8464aa411296f749ad436515cd6d78c925d711
Arijit Modak <arijitmo+Zeta@zeta.tech>
2025-08-04 17:12:42 +0530

    base app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const myName = ref('Arijit M.');
+
+</script>
+
+<template>
+    <input type = "text" v-bind:value = "myName" />
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+    text-decoration: underline;
+    background-color: blue;
+  }
+
+</style>
